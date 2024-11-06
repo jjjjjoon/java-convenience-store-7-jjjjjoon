b871770a72c2f6d0b4fc3e9d23a88e208eadbe12
jjjjjoon <mjj7343@gmail.com>
2024-11-06 23:39:57 +0900

    feat(InputView): 상품명, 구매수량 입력 메소드 생성

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,24 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private enum Message{
+        INPUT_NAME_NUMBER("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+
+        private String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public String getNameAndNumber() {
+        System.out.println(Message.INPUT_NAME_NUMBER.getMessage());
+        return Console.readLine();
+    }
+}
