f171bf46cf42da3b45a7610098e508f90ef1922d
jjjjjoon <mjj7343@gmail.com>
2024-11-07 17:25:16 +0900

    feat(OutputView): 어서오세요 메세지 출력 메소드 생성

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,4 +1,7 @@
 package store.view;
 
 public class OutputView {
+    public void printWelcomeMessage() {
+        System.out.println(ViewMessage.OUTPUT_WELCOME_MESSAGE.getMessage());
+    }
 }
--- src/main/java/store/view/ViewMessage.java
+++ src/main/java/store/view/ViewMessage.java
@@ -1,13 +1,13 @@
 package store.view;
 
-public enum Message{
+public enum ViewMessage {
     INPUT_NAME_NUMBER("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     OUTPUT_WELCOME_MESSAGE("안녕하세요. 우아한 편의점입니다."),
     OUTPUT_OUR_PRODUCTS("현재 보유하고 있는 상품입니다.");
 
     private String message;
 
-    Message(String message) {
+    ViewMessage(String message) {
         this.message = message;
     }
 
