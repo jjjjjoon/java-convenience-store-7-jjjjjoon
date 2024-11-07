18cbc604305a7c777fb955cace1ed923374b4831
jjjjjoon <mjj7343@gmail.com>
2024-11-07 17:23:25 +0900

    feat(Message): enum클래스 생성

---
+++ src/main/java/store/view/Message.java
@@ -0,0 +1,17 @@
+package store.view;
+
+public enum Message{
+    INPUT_NAME_NUMBER("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
+    OUTPUT_WELCOME_MESSAGE("안녕하세요. 우아한 편의점입니다."),
+    OUTPUT_OUR_PRODUCTS("현재 보유하고 있는 상품입니다.");
+
+    private String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
