e65f7162b561f239f7a16c80e129a3d60e2a11d5
jjjjjoon <mjj7343@gmail.com>
2024-11-10 02:42:04 +0900

    refactor(MembershipDiscount): 원시값 포장

--- src/main/java/store/service/MembershipDiscount.java
+++ src/main/java/store/service/MembershipDiscount.java
@@ -2,10 +2,10 @@ package store.service;
 
 import store.constant.Constants;
 
-public class MembershipDiscount {// 최대 할인 한도
+public class MembershipDiscount {
 
-    public int calculateDiscount(int amount) {
-        int discount = (int) (amount * Constants.DISCOUNT_RATE);  // 30% 할인 계산
+    public Integer calculateDiscount(Integer amount) {
+        Integer discount = (int) (amount * Constants.DISCOUNT_RATE);  // 30% 할인 계산
         return Math.min(discount, Constants.MAX_DISCOUNT_AMOUNT);  // 최대 할인 한도 적용
     }
 }
