52a567c3818c2b8049c22e072c929f6ae5e1f9d1
jjjjjoon <mjj7343@gmail.com>
2024-11-11 22:16:22 +0900

    feat(DiscountInfoDTO): 할인 관련 데이터 전달 dto 생성

--- src/main/java/store/dto/DiscountInfoDTO.java
+++ src/main/java/store/dto/DiscountInfoDTO.java
@@ -1,4 +1,33 @@
 package store.dto;
 
 public class DiscountInfoDTO {
+    private Integer totalAmount;
+    private Integer membershipDiscountAmount;
+    private Integer eventDiscountAmount;
+
+    public DiscountInfoDTO(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+        this.membershipDiscountAmount = 0;
+        this.eventDiscountAmount = 0;
+    }
+
+    public void applyMembershipDiscount(Integer discountAmount) {
+        this.membershipDiscountAmount = discountAmount;
+    }
+
+    public void applyEventDiscount(Integer discountAmount) {
+        this.eventDiscountAmount = discountAmount;
+    }
+
+    public Integer getFinalAmount() {
+        return totalAmount - membershipDiscountAmount - eventDiscountAmount;
+    }
+
+    public Integer getMembershipDiscountAmount() {
+        return membershipDiscountAmount;
+    }
+
+    public Integer getEventDiscountAmount() {
+        return eventDiscountAmount;
+    }
 }
