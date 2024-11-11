c1de8a597b7216a1bed9fc49667ecd38c3c96ea5
jjjjjoon <mjj7343@gmail.com>
2024-11-11 22:55:52 +0900

    refactor(OutputView): 메소드 분리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -8,6 +8,8 @@ import store.dto.ProductDisplayDTO;
 
 public class OutputView {
 
+    private final NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+
     public void printWelcomeMessage() {
         System.out.println(Message.OUTPUT_WELCOME_MESSAGE.getMessage());
         System.out.println(Message.OUTPUT_OUR_PRODUCTS.getMessage());
@@ -15,40 +17,58 @@ public class OutputView {
     }
 
     public void printCurrentProducts(List<ProductDisplayDTO> products) {
-        NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+        products.forEach(this::printProduct);
+        System.out.println();
+    }
 
-        for (ProductDisplayDTO product : products) {
-            String priceFormatted = formatter.format(product.getPrice());
+    private void printProduct(ProductDisplayDTO product) {
+        String priceFormatted = formatter.format(product.getPrice());
 
-            if (product.getQuantity() == 0) {
-                System.out.printf(Message.PRINT_NO_QUANTITY_LIST.getMessage(), product.getName(), priceFormatted);
-            } else {
-                String quantityFormatted = formatter.format(product.getQuantity());
-                System.out.printf(Message.DEFAULT_PRINT_PRODUCT_LIST.getMessage(), product.getName(), priceFormatted, quantityFormatted);
-            }
+        if (product.getQuantity() == 0) {
+            printNoQuantityProduct(product, priceFormatted);
+            return;
+        }
 
-            if (product.getPromotion() != null) {
-                System.out.print(Message.SPACE.getMessage() + product.getPromotion());
-            }
-            System.out.println();
+        printProductWithQuantity(product, priceFormatted);
+    }
+
+    private void printNoQuantityProduct(ProductDisplayDTO product, String priceFormatted) {
+        System.out.printf(Message.PRINT_NO_QUANTITY_LIST.getMessage(), product.getName(), priceFormatted);
+    }
+
+    private void printProductWithQuantity(ProductDisplayDTO product, String priceFormatted) {
+        String quantityFormatted = formatter.format(product.getQuantity());
+        System.out.printf(Message.DEFAULT_PRINT_PRODUCT_LIST.getMessage(), product.getName(), priceFormatted, quantityFormatted);
+
+        if (product.getPromotion() != null) {
+            printPromotion(product);
         }
         System.out.println();
     }
 
-    public void printReceipt(List<ProductDisplayDTO> items, Integer totalAmount, Integer eventDiscount, Integer membershipDiscount, Integer finalAmount) {
-        NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+    private void printPromotion(ProductDisplayDTO product) {
+        System.out.print(Message.SPACE.getMessage() + product.getPromotion());
+    }
 
+    public void printReceipt(List<ProductDisplayDTO> items, Integer totalAmount, Integer eventDiscount, Integer membershipDiscount, Integer finalAmount) {
         System.out.println(Message.WOOWA_CONVENIENCE_STORE.getMessage());
         System.out.println(Message.RECEIPT_HEADER.getMessage());
-        for (ProductDisplayDTO item : items) {
-            String itemTotalPrice = formatter.format(item.getPrice() * item.getQuantity());
-            System.out.printf(Message.RECEIPT_BODY.getMessage(), item.getName(), item.getQuantity(), itemTotalPrice);
-        }
-        System.out.println(Message.RECEIPT_PROMOTION_HEADER.getMessage());
-        // 증정 상품을 출력하는 코드 추가 가능 (필요 시)
 
+        items.forEach(this::printReceiptItem);
+
+        System.out.println(Message.RECEIPT_PROMOTION_HEADER.getMessage());
         System.out.println(Message.RECEIPT_DIVIDE_LINE.getMessage());
-        System.out.printf(Message.RECEIPT_TOTAL_AMOUNT.getMessage(), items.size(), formatter.format(totalAmount));
+
+        printReceiptSummary(items.size(), totalAmount, eventDiscount, membershipDiscount, finalAmount);
+    }
+
+    private void printReceiptItem(ProductDisplayDTO item) {
+        String itemTotalPrice = formatter.format(item.getPrice() * item.getQuantity());
+        System.out.printf(Message.RECEIPT_BODY.getMessage(), item.getName(), item.getQuantity(), itemTotalPrice);
+    }
+
+    private void printReceiptSummary(int itemCount, Integer totalAmount, Integer eventDiscount, Integer membershipDiscount, Integer finalAmount) {
+        System.out.printf(Message.RECEIPT_TOTAL_AMOUNT.getMessage(), itemCount, formatter.format(totalAmount));
         System.out.printf(Message.RECEIPT_EVENT_DISCOUNT.getMessage(), formatter.format(eventDiscount));
         System.out.printf(Message.RECEIPT_MEMBERSHIP_DISCOUNT.getMessage(), formatter.format(membershipDiscount));
         System.out.printf(Message.RECEIPT_FINAL_AMOUNT.getMessage(), formatter.format(finalAmount));
