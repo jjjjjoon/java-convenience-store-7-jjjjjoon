01bf8c50bf76107899cfa67b9d1bfc5a8ee06906
jjjjjoon <mjj7343@gmail.com>
2024-11-11 22:21:36 +0900

    feat(application): 전체 기능 구현 1차 완료
    영수증 출력 로직 수정 필요

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.Controller.ConvenienceStoreController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ConvenienceStoreController controller = new ConvenienceStoreController();
+        controller.run();
     }
 }
